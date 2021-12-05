eff0538d6b1615e74dd972d2c29a1a0ad502e0a3
Toad-stool <endorphe@gmail.com>
2021-12-05 17:35:01 +0800

    Add files via upload

---
+++ 7.sh
@@ -0,0 +1,26 @@
+shopt -s expand_aliases
+echo -n "Enter your IP:"
+read ip
+echo -n "Enter your domain:"
+read dm
+echo -n "Enter your password:"
+read pw
+apt install -y sudo
+sudo apt update
+sudo apt upgrade -y
+sudo apt install socat cron curl libcap2-bin xz-utils nano -y
+sudo systemctl start cron
+sudo mkdir -p /usr/local/etc/certfiles
+sudo curl https://get.acme.sh | sh
+alias acme.sh=~/.acme.sh/acme.sh
+acme.sh --set-default-ca  --server  letsencrypt
+acme.sh -d ${dm} --standalone --issue --force
+acme.sh --install-cert -d ${dm} --key-file /usr/local/etc/certfiles/private.key --fullchain-file /usr/local/etc/certfiles/certificate.crt --force
+acme.sh  --upgrade  --auto-upgrade
+sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
+sudo sed -i "s:password1:${pw}:g" /usr/local/etc/trojan/config.json
+sudo sed -i "s:password2:${pw}:g" /usr/local/etc/trojan/config.json
+sudo sed -i "s:/path/to/certificate.crt:/usr/local/etc/certfiles/certificate.crt:g" /usr/local/etc/trojan/config.json
+sudo sed -i "s:/path/to/private.key:/usr/local/etc/certfiles/private.key:g" /usr/local/etc/trojan/config.json
+lsmod | grep bbr
+echo "Trojan部署完成，请重启VPS"

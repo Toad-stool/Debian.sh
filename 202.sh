b402775f259711e73e85956eed473ef8eb2f6736
Toad-stool <endorphe@gmail.com>
2021-12-07 19:19:14 +0800

    Rename 202.sh to History/202.sh

--- 202.sh
+++
@@ -1,4 +0,0 @@
-apt install -y sudo
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install socat cron curl libcap2-bin xz-utils nano -y
---
+++ History/202.sh
@@ -0,0 +1,4 @@
+apt install -y sudo
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install socat cron curl libcap2-bin xz-utils nano -y
